0ac301e5e727c921d55bb50f79af8f48c2e55288
letung53 <lehoangtung7629@gmail.com>
2024-11-28 07:20:52 +0700

    Complete Assignment

--- OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
+++ OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class GarbageCreator {
     public static void main(String[] args) throws IOException {
-        String filename = "C:\\softwares\\projectJava\\IT3100E.744517.20241.20235508.NguyenXuanKhai\\src\\OtherProjects\\hust\\soict\\dsai\\garbage\\bin.txt";
+        String filename = "/home/bop/Desktop/Untitled Folder/OtherProjects/src/hust/soict/dsai/garbage/bin.txt";
         byte[] inputBytes = { 0 };
         long startTime, endTime;
         inputBytes = Files.readAllBytes(Paths.get(filename));
--- OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
+++ OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class NoGarbage {
     public static void main(String[] args) throws IOException {
-        String filename = "C:\\softwares\\projectJava\\IT3100E.744517.20241.20235508.NguyenXuanKhai\\OtherProjects\\src\\hust\\soict\\dsai\\garbage\\bin.txt";
+        String filename = "/home/bop/Desktop/Untitled Folder/OtherProjects/src/hust/soict/dsai/garbage/bin.txt";
         byte[] inputBytes = { 0 };
         long startTime, endTime;
 
